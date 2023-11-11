925ada98bfd9888782e19f2b36740c95b375c8b8
Riccardo de Lutio <rdelutio@nvidia.com>
2023-11-11 02:54:25 -0800

    account for empty logs

--- result_parser.py
+++ result_parser.py
@@ -29,32 +29,35 @@ if __name__ == "__main__":
     os.makedirs(args.out_dir, exist_ok=True)
     for run in runs:
         run_path = os.path.join(tensorboard_path,run)
+        
+        conf_path = os.path.join(run_path,'parsed.yaml')
+        if os.path.exists(conf_path):
+            with open(conf_path, 'r') as file:
+                conf = yaml.safe_load(file)
 
-        with open(os.path.join(run_path,'parsed.yaml'), 'r') as file:
-            conf = yaml.safe_load(file)
+            relative_path = []
+            for a, b in zip_longest(conf['path'].split('/'), REMOTE_WORKSPACE_DATA_PATH.split('/')):
+                if a != b:
+                    relative_path.append(a if a is not None else b)
+            local_data_path = os.path.join(args.local_data_path,"/".join(relative_path))
 
-        relative_path = []
-        for a, b in zip_longest(conf['path'].split('/'), REMOTE_WORKSPACE_DATA_PATH.split('/')):
-            if a != b:
-                relative_path.append(a if a is not None else b)
-        local_data_path = os.path.join(args.local_data_path,"/".join(relative_path))
+            ls_dir = sorted(os.listdir(run_path))
+            if len(ls_dir)>0:
+                filtered_checkpoints = list(filter(lambda x: x.endswith('.pt'), ls_dir))
+                epochs = [int(checkpoint.split('.')[0].split('_')[1]) for checkpoint in filtered_checkpoints]
 
-        ls_dir = sorted(os.listdir(run_path))
-        filtered_checkpoints = list(filter(lambda x: x.endswith('.pt'), ls_dir))
-        epochs = [int(checkpoint.split('.')[0].split('_')[1]) for checkpoint in filtered_checkpoints]
+                last_checkpoint = filtered_checkpoints[np.argmax(epochs)]
 
-        last_checkpoint = filtered_checkpoints[np.argmax(epochs)]
-
-        print()
-        print(f"Rendering for experiment {args.experiment_name}, run {run}, checkpoint {last_checkpoint}")
-        renderer = Renderer(checkpoint_path=os.path.join(run_path,last_checkpoint), 
-                            out_dir=os.path.join(args.out_dir,run),
-                            path=local_data_path,
-                            save_gt=args.save_gt)
-        mean, std = renderer.render_all()
-        psnrs.append(mean)
-        stds.append(std)
-        checkpoints.append(last_checkpoint)
+                print()
+                print(f"Rendering for experiment {args.experiment_name}, run {run}, checkpoint {last_checkpoint}")
+                renderer = Renderer(checkpoint_path=os.path.join(run_path,last_checkpoint), 
+                                    out_dir=os.path.join(args.out_dir,run),
+                                    path=local_data_path,
+                                    save_gt=args.save_gt)
+                mean, std = renderer.render_all()
+                psnrs.append(mean)
+                stds.append(std)
+                checkpoints.append(last_checkpoint)
 
 
     print(f"\n\nExperiment {args.experiment_name} summary:\n")
