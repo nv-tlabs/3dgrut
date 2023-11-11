ed3d47b2a4e878d8c4b10b2216d0068822d7db64
Riccardo de Lutio <rdelutio@nvidia.com>
2023-11-11 02:42:50 -0800

    add script to parse NGC results, run test at the end of training with the latest checkpoint

--- render.py
+++ render.py
@@ -17,6 +17,96 @@ from torchmetrics import PeakSignalNoiseRatio
 
 BACKGROUND_COLOR = torch.zeros((3,), dtype=torch.float32, device='cuda')
 
+
+class Renderer:
+    def __init__(self, checkpoint_path, out_dir, path="", save_gt=True, writer=None) -> None:
+        self.out_dir = out_dir
+        self.path = path
+        self.save_gt = save_gt
+
+
+        self.checkpoint = torch.load(checkpoint_path)
+        self.conf = self.checkpoint["config"]
+        self.global_step = self.checkpoint['global_step']
+
+        # Replace the path to the test data
+        if self.path: 
+            self.conf.path = path
+
+        val_collate_fn = None
+        # Create the dataset
+        if self.conf.dataset.type == 'nerf':
+            dataset = NeRFDataset(
+                self.conf.path, 
+                split='test', 
+                sample_full_image=True, 
+                batch_size=1,
+                return_alphas=True
+            )
+        else:
+            raise ValueError(f'Unsupported dataset type: {self.conf.dataset.type}. Choose between: ["colmap", "nerf", "ngp", "ncore"]. ')
+        
+        self.dataloader = torch.utils.data.DataLoader(dataset, num_workers=8, batch_size=1, shuffle=False, collate_fn=val_collate_fn)
+
+        # Initialize the model and the optix context
+        self.model = MixtureOfGaussians(self.conf)
+        self.model.set_optix_context()
+
+
+        # Initialize the parameters from checkpoint
+        self.model.init_from_checkpoint(self.checkpoint)
+        self.model.build_bvh()
+
+        self.writer = writer
+        
+    def render_all(self):
+        # Criterions that we log during training
+        criterions = {"psnr":  PeakSignalNoiseRatio(data_range=1).to("cuda")}
+
+        output_path_renders = os.path.join(self.out_dir, f"ours_{int(self.global_step)}", "renders")
+        os.makedirs(output_path_renders, exist_ok=True)
+
+        if self.save_gt:
+            output_path_gt = os.path.join(self.out_dir, f"ours_{int(self.global_step)}", "gt")
+            os.makedirs(output_path_gt, exist_ok=True)
+        
+        psnr = []
+        iteration = 0
+        with tqdm(self.dataloader) as pbar:
+            for batch in pbar:
+                with torch.no_grad():
+                    gpu_batch = move_to_gpu(batch)
+                    rays_ori, rays_dir, rgb_gt = gpu_batch["rays_ori"], gpu_batch["rays_dir"], gpu_batch["rgb_gt"]
+                    # Compute the outputs of a single batch
+                    outputs = self.model(rays_ori, rays_dir)
+                    
+                    # The values are already alpha composited with the background
+                    rgb_pred = outputs['pred_rgb']
+                    torchvision.utils.save_image(rgb_pred.squeeze(0).permute(2,0,1), os.path.join(output_path_renders, '{0:05d}'.format(iteration) + ".png"))
+                    if self.writer is not None:
+                        self.writer.add_image('image/test', outputs['pred_rgb'][-1].clip(0,1.0), self.global_step, dataformats='HWC')
+
+
+                    if self.save_gt:
+                        if "alpha" in gpu_batch: 
+                            rgb_gt = rgb_gt * gpu_batch["alpha"] + BACKGROUND_COLOR * (1 - gpu_batch["alpha"])
+                        torchvision.utils.save_image(rgb_gt.squeeze(0).permute(2,0,1), os.path.join(output_path_gt, '{0:05d}'.format(iteration) + ".png"))
+
+                    # Compute the loss
+                    psnr.append(criterions["psnr"](rgb_pred, rgb_gt).item())
+
+                    pbar.set_postfix({'iteration': iteration, 'psnr': psnr[-1]})
+                    iteration += 1
+
+            mean_psnr = np.mean(psnr)
+            std_psnr = np.std(psnr)
+
+            print(f"PSNR : {mean_psnr}, std: f{std_psnr}")
+            
+            if self.writer is not None:
+                self.writer.add_scalar("psnr/test", mean_psnr, self.global_step)
+            return mean_psnr, std_psnr
+
 if __name__ == "__main__":
     # Set up command line argument parser
     parser = argparse.ArgumentParser()
@@ -26,69 +116,9 @@ if __name__ == "__main__":
     parser.add_argument("--save-gt", action="store_false", help="If set, the GT images will not be saved [True by default]")
     args = parser.parse_args()
 
+    renderer = Renderer(checkpoint_path=args.checkpoint,
+                        path=args.path,
+                        out_dir=args.out_dir,
+                        save_gt=args.save_gt)
 
-    checkpoint = torch.load(args.checkpoint)
-    conf = checkpoint["config"]
-    global_step = checkpoint['global_step']
-
-    # Replace the path to the test data
-    if args.path: 
-        conf.path = args.path
-
-    val_collate_fn = None
-    # Create the dataset
-    if conf.dataset.type == 'nerf':
-        dataset = NeRFDataset(
-            conf.path, 
-            split='test', 
-            sample_full_image=True, 
-            batch_size=1,
-            return_alphas=True
-        )
-    else:
-        raise ValueError(f'Unsupported dataset type: {conf.dataset.type}. Choose between: ["colmap", "nerf", "ngp", "ncore"]. ')
-    
-    dataloader = torch.utils.data.DataLoader(dataset, num_workers=8, batch_size=1, shuffle=False, collate_fn=val_collate_fn)
-
-    # Initialize the model and the optix context
-    model = MixtureOfGaussians(conf)
-    model.set_optix_context()
-
-    # Initialize the parameters from checkpoint
-    model.init_from_checkpoint(checkpoint)
-    model.build_bvh()
-    # Criterions that we log during training
-    criterions = {"psnr":  PeakSignalNoiseRatio(data_range=1).to("cuda")}
-
-    output_path_renders = os.path.join(args.out_dir, f"ours_{int(global_step)}", "renders")
-    os.makedirs(output_path_renders, exist_ok=True)
-
-    if args.save_gt:
-        output_path_gt = os.path.join(args.out_dir, f"ours_{int(global_step)}", "gt")
-        os.makedirs(output_path_gt, exist_ok=True)
-    
-    psnr = []
-    iteration = 0
-    with tqdm(dataloader) as pbar:
-        for batch in pbar:
-            with torch.no_grad():
-                gpu_batch = move_to_gpu(batch)
-                rays_ori, rays_dir, rgb_gt = gpu_batch["rays_ori"], gpu_batch["rays_dir"], gpu_batch["rgb_gt"]
-                # Compute the outputs of a single batch
-                outputs = model(rays_ori, rays_dir)
-                
-                # The values are already alpha composited with the background
-                rgb_pred = outputs['pred_rgb']
-                torchvision.utils.save_image(rgb_pred.squeeze(0).permute(2,0,1), os.path.join(output_path_renders, '{0:05d}'.format(iteration) + ".png"))
-
-                if args.save_gt:
-                    if "alpha" in gpu_batch: 
-                        rgb_gt = rgb_gt * gpu_batch["alpha"] + BACKGROUND_COLOR * (1 - gpu_batch["alpha"])
-                    torchvision.utils.save_image(rgb_gt.squeeze(0).permute(2,0,1), os.path.join(output_path_gt, '{0:05d}'.format(iteration) + ".png"))
-
-                # Compute the loss
-                psnr.append(criterions["psnr"](rgb_pred, rgb_gt).item())
-                pbar.set_postfix({'iteration': iteration, 'psnr': psnr[-1]})
-                iteration += 1
-
-        print(f"PSNR : {np.mean(psnr)}, std: f{np.std(psnr)}")
+    renderer.render_all()
---
+++ result_parser.py
@@ -0,0 +1,65 @@
+import os
+import argparse
+
+import yaml
+from itertools import zip_longest
+import numpy as np
+
+from render import Renderer
+
+REMOTE_WORKSPACE_DATA_PATH = "/workspace/data"
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--workspace-path", required=True, type=str, help="Path to the workspace")
+    parser.add_argument("--experiment-name", required=True, type=str, help="Experiment name")
+    parser.add_argument("--out-dir", required=True, type=str, help="Output path")
+    parser.add_argument("--log-dir", type=str, default="runs", help="Tensorbord log dir")
+    parser.add_argument("--local-data-path", type=str, default="/scratch/data/3dgrt_data", help="local data dir")
+    parser.add_argument("--save-gt", action="store_false", help="If set, the GT images will not be saved [True by default]")
+    args = parser.parse_args()
+
+    tensorboard_path = os.path.join(args.workspace_path,"experiments",args.experiment_name,args.log_dir)
+    runs = os.listdir(tensorboard_path)
+
+    psnrs = []
+    stds = []   
+    checkpoints = []
+    os.makedirs(args.out_dir, exist_ok=True)
+    for run in runs:
+        run_path = os.path.join(tensorboard_path,run)
+
+        with open(os.path.join(run_path,'parsed.yaml'), 'r') as file:
+            conf = yaml.safe_load(file)
+
+        relative_path = []
+        for a, b in zip_longest(conf['path'].split('/'), REMOTE_WORKSPACE_DATA_PATH.split('/')):
+            if a != b:
+                relative_path.append(a if a is not None else b)
+        local_data_path = os.path.join(args.local_data_path,"/".join(relative_path))
+
+        ls_dir = sorted(os.listdir(run_path))
+        filtered_checkpoints = list(filter(lambda x: x.endswith('.pt'), ls_dir))
+        epochs = [int(checkpoint.split('.')[0].split('_')[1]) for checkpoint in filtered_checkpoints]
+
+        last_checkpoint = filtered_checkpoints[np.argmax(epochs)]
+
+        print()
+        print(f"Rendering for experiment {args.experiment_name}, run {run}, checkpoint {last_checkpoint}")
+        renderer = Renderer(checkpoint_path=os.path.join(run_path,last_checkpoint), 
+                            out_dir=os.path.join(args.out_dir,run),
+                            path=local_data_path,
+                            save_gt=args.save_gt)
+        mean, std = renderer.render_all()
+        psnrs.append(mean)
+        stds.append(std)
+        checkpoints.append(last_checkpoint)
+
+
+    print(f"\n\nExperiment {args.experiment_name} summary:\n")
+    for psnr, std, run, checkpoint in zip(psnrs, stds, runs, checkpoints):
+        print(f"Run: {run}, checkpoint {checkpoint}")
+        print(f"PSNR: {psnr} std {std}\n")
+
+    
