889216bb9e0dda5df11d618f51fd24219bf674d7
Zan Gojcic <zgojcic@nvidia.com>
2023-11-11 03:08:15 +0100

    seperate gui out of the train.py

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ from model import MixtureOfGaussians
 from background import BackgroundColor
 from datasets.utils import move_to_gpu
 from loss_utils import ssim
-from utils import to_np
+from gui import GUI
 from recorder import TrainingRecorder
 sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 
@@ -168,179 +168,10 @@ def main(conf: DictConfig) -> None:
         model.setup_optimizer()
         global_step = 0
 
+    gui = None
     if conf.with_gui:
         import polyscope as ps
-        import polyscope.imgui as psim
-
-        ps.set_use_prefs_file(False)
-
-        if conf.dataset.type == 'nerf': # NeRF synthetic uses the blender coordinate-system
-            ps.set_up_dir("z_up")
-            ps.set_front_dir("neg_y_front")
-            ps.set_navigation_style("turntable")
-        elif conf.dataset.type == 'colmap': # Colmap scenes use a cartesian coordinate-system
-            ps.set_up_dir("neg_y_up")
-            ps.set_front_dir("neg_z_front")
-            ps.set_navigation_style("free")
-        else:                              # AV use cartesian coordinate-system with z-up
-            ps.set_up_dir("z_up")
-            ps.set_front_dir("x_front")
-            ps.set_navigation_style("free")
-
-        ps.set_enable_vsync(False)
-        ps.set_max_fps(-1)
-        ps.set_background_color((0., 0., 0.))
-        ps.set_ground_plane_mode("none")
-        ps.set_window_resizable(True)
-        ps.set_window_size(1920, 1080)
-        ps.set_give_focus_on_show(True)
-        
-        ps.set_automatically_compute_scene_extents(False)
-        ps.set_bounding_box(to_np(scene_bbox[0]), to_np(scene_bbox[1]))
-
-        # viz stateful parameters & options
-        viz_do_train = False
-        viz_bbox = False
-        viz_skip_update = False  # if enabled, will skip rendering updates to accelerate background training loop
-        viz_render_styles = ['color', 'density']
-        viz_render_style_ind = 0
-        viz_curr_render_size = None
-        viz_curr_render_style_ind = None
-        viz_render_color_buffer = None
-        viz_render_scalar_buffer = None
-        viz_render_name = 'render'
-        
-        ps.init()
-
-        ps_point_cloud = ps.register_point_cloud("centers", to_np(model.get_positions()), 
-                                radius=1e-3, point_render_mode='quad')
-        ps_point_cloud_buffer = ps_point_cloud.get_buffer("points")
-
-        # Only implemented for NeRF and Colmap dataset
-        if isinstance(train_dataset, (NeRFDataset, ColmapDataset)):
-            train_dataset.create_dataset_camera_visualization()
-            val_dataset.create_dataset_camera_visualization()
-        
-        def update_cloud_viz():
-            nonlocal ps_point_cloud, ps_point_cloud_buffer
-
-            # re-initialize the viz
-            if ps_point_cloud is None or ps_point_cloud.n_points() != model.get_positions().shape[0]:
-                ps_point_cloud = ps.register_point_cloud("centers", to_np(model.get_positions()))
-                ps_point_cloud_buffer = ps_point_cloud.get_buffer("points")
-
-            # direct on-GPU update, must not have changed size
-            ps_point_cloud_buffer.update_data_from_device(model.get_positions().detach())
-
-        def render_from_current_ps_view():
-
-            window_w, window_h = ps.get_window_size()
-            view_params = ps.get_view_camera_parameters()
-            cam_center = view_params.get_position()
-            corner_rays = view_params.generate_camera_ray_corners()
-            c_ul, c_ur, c_ll, c_lr = [torch.tensor(a, device=DEFAULT_DEVICE, dtype=torch.float32) for a in corner_rays]
-
-            # generate view camera ray origins and directions
-            rays_ori = torch.tensor(cam_center, device=DEFAULT_DEVICE, dtype=torch.float32).reshape(1,1,1,3).expand(1,window_h,window_w,3)
-            interp_x, interp_y= torch.meshgrid(
-                            torch.linspace(0., 1., window_w, device=DEFAULT_DEVICE, dtype=torch.float32),
-                            torch.linspace(0., 1., window_h, device=DEFAULT_DEVICE, dtype=torch.float32),
-                            indexing='xy')
-            interp_x = interp_x.unsqueeze(-1)
-            interp_y = interp_y.unsqueeze(-1)
-            rays_dir = c_ul + interp_x * (c_ur - c_ul) + interp_y * (c_ll - c_ul)
-            rays_dir = torch.nn.functional.normalize(rays_dir, dim=-1)
-            rays_dir = rays_dir.unsqueeze(0)
-
-            # Render a frame
-            with torch.no_grad():
-                outputs = model(rays_ori, rays_dir)
-
-            return outputs['pred_rgb'], outputs['pred_opacity'], outputs['pred_ohit']
-
-        def update_render_view_viz():
-            nonlocal viz_curr_render_size, viz_curr_render_style_ind, viz_render_color_buffer, viz_render_scalar_buffer
-
-            window_w, window_h = ps.get_window_size()
-
-            # re-initialize if needed
-            style = viz_render_styles[viz_render_style_ind]
-            if viz_curr_render_style_ind != viz_render_style_ind or viz_curr_render_size != (window_w, window_h):
-           
-                viz_curr_render_style_ind = viz_render_style_ind
-                viz_curr_render_size = (window_w, window_h)
-
-                if style == "color":
-
-                    dummy_image = np.ones((window_h, window_w, 4), dtype=np.float32)
-
-                    ps.add_color_alpha_image_quantity(
-                        viz_render_name,
-                        dummy_image,
-                        enabled=True,
-                        image_origin="upper_left",
-                        show_fullscreen=True,
-                        show_in_imgui_window=False,
-                    )
-
-                    viz_render_color_buffer = ps.get_quantity_buffer(viz_render_name, "colors")
-                    viz_render_scalar_buffer = None
-                
-                elif style == "density":
-                
-                    dummy_vals = np.zeros((window_h, window_w), dtype=np.float32)
-                    dummy_vals[0] = 1.0  # hack so the default polyscope scale gets set more nicely
-
-                    ps.add_scalar_image_quantity(
-                        viz_render_name,
-                        dummy_vals,
-                        enabled=True,
-                        image_origin="upper_left",
-                        show_fullscreen=True,
-                        show_in_imgui_window=False,
-                        cmap="blues",
-                        vminmax=(0, 1),
-                    )
-
-                    viz_render_color_buffer = None
-                    viz_render_scalar_buffer = ps.get_quantity_buffer(viz_render_name, "values")
-
-
-            if viz_bbox: 
-                bbox_min, bbox_max = train_dataset.scene_bbox
-                nodes = np.array([[bbox_min[0], bbox_min[1], bbox_min[2]], [bbox_max[0], bbox_min[1], bbox_min[2]], [bbox_min[0], bbox_max[1], bbox_min[2]], 
-                                  [bbox_min[0], bbox_min[1], bbox_max[2]], [bbox_max[0], bbox_max[1], bbox_min[2]], [bbox_max[0], bbox_min[1], bbox_max[2]], 
-                                  [bbox_min[0], bbox_max[1], bbox_max[2]], [bbox_max[0], bbox_max[1], bbox_max[2]]])
-                edges = np.array(
-                    [[0, 1], [0, 2], [0, 3], [1, 4], [1, 5], [2, 6], [2, 4], [3, 5], [3, 6], [4, 7], [5, 7], [6, 7]])
-                ps.register_curve_network("bbox", nodes, edges)
-
-            # do the actual rendering
-            sple_orad, sple_odns, sple_ohit = render_from_current_ps_view()
-
-            # update the data
-            if style == "color":
-                # append 1s for alpha
-                sple_orad = torch.cat((sple_orad, torch.ones_like(sple_orad[:,:,:,0:1])), dim=-1)
-                viz_render_color_buffer.update_data_from_device(sple_orad.detach())
-
-            elif style == "density":
-                viz_render_scalar_buffer.update_data_from_device(sple_odns.detach())
-
-
-        def ps_ui_callback():
-            nonlocal viz_do_train, viz_skip_update, viz_render_style_ind, viz_bbox
-            # Create a little ImGUI UI
-            _, viz_do_train = psim.Checkbox("Train", viz_do_train)
-            _, viz_skip_update = psim.Checkbox("Skip Render Update", viz_skip_update)
-            if isinstance(train_dataset, (NeRFDataset, ColmapDataset)):
-                _, viz_bbox = psim.Checkbox("Visualize the bbox", viz_bbox)
-            _, viz_render_style_ind = psim.Combo("Render Display", viz_render_style_ind, viz_render_styles)
-
-            if not viz_skip_update:
-                update_render_view_viz()
-
-        ps.set_user_callback(ps_ui_callback)
+        gui = GUI(conf, model, train_dataset, val_dataset)
 
     n_epochs = int(n_iterations/train_dataset.__len__())
 
@@ -486,15 +317,15 @@ def main(conf: DictConfig) -> None:
                 if scene_updated or (global_step > 0 and conf.model.bvh_update_frequency > 0 and global_step % conf.model.bvh_update_frequency == 0):
                     model.build_bvh()
 
-                if conf.with_gui:
-                    if not viz_skip_update:
+                if gui is not None:
+                    if not gui.viz_skip_update:
                         if scene_updated or model.get_positions().requires_grad:
-                            update_cloud_viz()
+                            gui.update_cloud_viz()
             
-                        update_render_view_viz()
+                        gui.update_render_view_viz()
 
                     ps.frame_tick()
-                    while not viz_do_train:
+                    while not gui.viz_do_train:
                         ps.frame_tick()
 
     recorder.submit_recording(
