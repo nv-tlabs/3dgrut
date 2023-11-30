2f36477f315b5022099dfd607250ab80c921b8d6
Riccardo de Lutio <rdelutio@nvidia.com>
2023-11-29 22:57:50 -0800

    fixes after rebase

--- train.py
+++ train.py
@@ -439,18 +439,19 @@ def main(conf: DictConfig) -> None:
                 # Update the BVH if required
                 if scene_updated or (global_step > 0 and conf.model.bvh_update_frequency > 0 and global_step % conf.model.bvh_update_frequency == 0):
                     model.build_bvh()
-                
+
+                # Updating the GUI
                 if gui is not None:
                     if gui.live_update:
                         if scene_updated or model.get_positions().requires_grad:
                             gui.update_cloud_viz()
-            
                         gui.update_render_view_viz()
 
                     ps.frame_tick()
                     while not gui.viz_do_train:
                         ps.frame_tick()
 
+                # Optimizer step
                 with torch.cuda.nvtx.range("backpropagation"):
                     model.optimizer.step()
                     model.optimizer.zero_grad()
@@ -470,7 +471,6 @@ def main(conf: DictConfig) -> None:
                 with torch.cuda.nvtx.range("backpropagation"):
                     model.optimizer.step()
                     model.optimizer.zero_grad()
-
 
                 # Update the error buffers with new values
                 if global_step > 0 and model.error_based_sampling and global_step % model.error_buffer_update_frequency == 0:
@@ -519,7 +519,7 @@ def main(conf: DictConfig) -> None:
                         model.update_error_buffer(torch.vstack(ray_origins), torch.vstack(ray_directions),
                                                 torch.vstack(errors), torch.vstack(rgbs),
                                                 torch.vstack(alphas) if len(alphas) else None)
-                        
+        
     recorder.submit_recording(
         dataset=train_dataset,
         scene_extent=scene_bbox,
