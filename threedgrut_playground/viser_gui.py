68cabfaf04398983051a497625b9287fa4d32d9d
orperel <or_perel@hotmail.com>
2025-06-19 20:00:09 -0400

    PR polishing after recent merges to main

--- threedgrut_playground/viser_gui.py
+++ threedgrut_playground/viser_gui.py
@@ -1,32 +1,23 @@
-from threading import Thread
-import torch
-import numpy as np
+import os
+import sys
 import time
+import numpy as np
+import torch
 try:
     import viser
     import viser.transforms as tf
 except ImportError:
     print('viser not installed, please run "pip install viser"')
+    sys.exit(1)
 import cv2
-from collections import deque
 import argparse
-
-import numpy as np
-import torch
-import torchvision.transforms.functional as F
 import kaolin
-
-#Below referenced threedgrut_playground code
-from threedgrut.utils.logger import logger
-from threedgrut.gui.ps_extension import initialize_cugl_interop
-from threedgrut_playground.utils.video_out import VideoRecorder
+from collections import deque
 from threedgrut_playground.engine import Engine3DGRUT, OptixPrimitiveTypes
-
 from kaolin.render.camera import Camera
 from threedgrut.utils.misc import quaternion_to_so3
 
-
-# Below code referenced viser https://github.com/nerfstudio-project/viser
+# Below code referenced from viser https://github.com/nerfstudio-project/viser
 # and viser 3dgs example: https://github.com/WangFeng18/3d-gaussian-splatting/blob/main/visergui.py
 
 def get_c2w(camera):
@@ -45,19 +36,11 @@ def get_w2c(camera):
     w2c = np.linalg.inv(c2w)
     return w2c
 
-# argparse for parameters
-def parse_args():
-    parser = argparse.ArgumentParser(description="Viser remote viewer for 3dgrut")
-    parser.add_argument('--gs_object', type=str, default="./runs/lego-3004_092246/ckpt_last.pt")
-    parser.add_argument('--mesh_assets_folder', type=str, default="./threedgrut_playground/assets")
-    parser.add_argument('--default_config', type=str, default="apps/colmap_3dgrt.yaml")
-    parser.add_argument('--port', type=int, default=8080)
-    parser.add_argument('--target_fps', type=float, default=20.0)
-    return parser.parse_args()
 
 class ViserViewer:
     def __init__(self, viewer_ip_port, engine, target_fps=20.0):
         self.engine = engine
+        self.set_initial_mesh()
         self.port = viewer_ip_port
         self.target_fps = target_fps
         self.render_times = deque(maxlen=3)
@@ -94,15 +77,20 @@ class ViserViewer:
             def _(_):
                 self.need_update = True
 
+    def set_initial_mesh(self):
+        """ Current implementation sets an empty scene with no meshes """
+        # The 3dgrut playground engine is loaded with a sample mesh primitive (glass sphere),
+        # remove the default mesh primitives from the engine here
+        for mesh_name in list(self.engine.primitives.objects.keys()):
+            self.engine.primitives.remove_primitive(mesh_name)
+
     def fast_render(self, in_cam, is_first_pass=False):
         # Called during interactions, disables effects for quick rendering
         framebuffer = self.engine.render_pass(in_cam, is_first_pass=is_first_pass)
         rgba_buffer = torch.cat([framebuffer['rgb'], framebuffer['opacity']], dim=-1)
         rgba_buffer = torch.clamp(rgba_buffer, 0.0, 1.0)
-        # return (rgba_buffer[0] * 255).to(torch.uint8)
         img = (rgba_buffer[0, :, :, :3] * 255).to(torch.uint8)  # [H, W, 3], RGB
         img_np = img.cpu().numpy()
-        # img_np = img_np[..., ::-1]  # RGB to BGR
         return img_np
 
     # refrenced https://github.com/nv-tlabs/3dgrut/blob/main/threedgrut_playground/ps_gui.py#L132
@@ -146,19 +134,49 @@ class ViserViewer:
             self.fps.value = f"{1.0 / np.mean(self.render_times):.3g}"
             self.need_update = False
 
+
 if __name__ == "__main__":
-    args = parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--gs_object',
+        type=str,
+        required=True,
+        help="Path of pretrained 3dgrt checkpoint, as .pt / .ingp / .ply file."
+    )
+    parser.add_argument(
+        '--mesh_assets',
+        type=str,
+        default=os.path.join(os.path.dirname(__file__), 'assets'),
+        help="Path to folder containing mesh assets of .obj or .glb format."
+    )
+    parser.add_argument(
+        '--default_gs_config',
+        type=str,
+        default='apps/colmap_3dgrt.yaml',
+        help="Name of default config to use for .ingp, .ply files, or .pt files not trained with 3dgrt."
+    )
+    parser.add_argument(
+        '--envmap_assets',
+        type=str,
+        default=os.path.join(os.path.dirname(__file__), 'assets'),
+        help="Optional path to folder containing .hdr environment maps to use for lighting mesh assets."
+    )
+    parser.add_argument('--port', type=int, default=8080)
+    parser.add_argument('--target_fps', type=float, default=20.0)
+    args = parser.parse_args()
+
     engine = Engine3DGRUT(
         gs_object=args.gs_object,
-        mesh_assets_folder=args.mesh_assets_folder,
-        default_config=args.default_config,
+        mesh_assets_folder=args.mesh_assets,
+        envmap_assets_folder=args.envmap_assets,
+        default_config=args.default_gs_config,
     )
     viewer = ViserViewer(viewer_ip_port=args.port, engine=engine, target_fps=args.target_fps)
     last_time = time.time()
     while True:
         start = time.time()
         viewer.update()
-        # 动态FPS控制
+        # Dynamic FPS Control
         elapsed = time.time() - start
         sleep_time = max(0, (1.0 / args.target_fps) - elapsed)
         time.sleep(sleep_time)
