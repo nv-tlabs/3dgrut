db0cee9aac004ebab70a0321ae726db5a63e6ee8
Qi Wu <qiwu@nvidia.com>
2025-07-02 22:27:50 +0000

    update

--- convert.sh
+++ convert.sh
@@ -1,11 +1,21 @@
 #!/bin/bash 
 
-python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/bicycle/bicycle-1706_190051/ckpt_last.pt 
-python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/bonsai/bonsai-1706_191220/ckpt_last.pt 
-python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/counter/counter-1706_192445/ckpt_last.pt 
-python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/flowers/flowers-1706_193859/ckpt_last.pt 
-python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/garden/garden-1706_194842/ckpt_last.pt 
-python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/kitchen/kitchen-1706_195754/ckpt_last.pt 
-python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/room/room-1706_201109/ckpt_last.pt 
-python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/stump/stump-1706_202300/ckpt_last.pt 
-python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/treehill/treehill-1706_203135/ckpt_last.pt 
+# python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/bicycle/bicycle-1706_190051/ckpt_last.pt 
+# python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/bonsai/bonsai-1706_191220/ckpt_last.pt 
+# python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/counter/counter-1706_192445/ckpt_last.pt 
+# python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/flowers/flowers-1706_193859/ckpt_last.pt 
+# python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/garden/garden-1706_194842/ckpt_last.pt 
+# python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/kitchen/kitchen-1706_195754/ckpt_last.pt 
+# python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/room/room-1706_201109/ckpt_last.pt 
+# python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/stump/stump-1706_202300/ckpt_last.pt 
+# python convert.py results/3dgut/mipnerf360_a6972c0_mcmc/treehill/treehill-1706_203135/ckpt_last.pt 
+
+python convert.py /home/qiwu/Work/3dgrt-external/results/3dgrt/mipnerf360_6cc521d/bicycle/bicycle-*/ckpt_last.pt 
+python convert.py /home/qiwu/Work/3dgrt-external/results/3dgrt/mipnerf360_6cc521d/bonsai/bonsai-*/ckpt_last.pt 
+python convert.py /home/qiwu/Work/3dgrt-external/results/3dgrt/mipnerf360_6cc521d/counter/counter-*/ckpt_last.pt 
+python convert.py /home/qiwu/Work/3dgrt-external/results/3dgrt/mipnerf360_6cc521d/flowers/flowers-*/ckpt_last.pt 
+python convert.py /home/qiwu/Work/3dgrt-external/results/3dgrt/mipnerf360_6cc521d/garden/garden-*/ckpt_last.pt 
+python convert.py /home/qiwu/Work/3dgrt-external/results/3dgrt/mipnerf360_6cc521d/kitchen/kitchen-*/ckpt_last.pt 
+python convert.py /home/qiwu/Work/3dgrt-external/results/3dgrt/mipnerf360_6cc521d/room/room-*/ckpt_last.pt 
+python convert.py /home/qiwu/Work/3dgrt-external/results/3dgrt/mipnerf360_6cc521d/stump/stump-*/ckpt_last.pt 
+python convert.py /home/qiwu/Work/3dgrt-external/results/3dgrt/mipnerf360_6cc521d/treehill/treehill-*/ckpt_last.pt 
