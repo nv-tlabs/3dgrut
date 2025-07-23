494c8ba3835ff0097c419adad075e56b605484ec
Qi Wu <qiwu@nvidia.com>
2025-07-23 10:16:26 -0700

    add training script

---
+++ run_scivis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+OUT_DIR=results/scivis
+
+export TORCH_EXTENSIONS_DIR=$OUT_DIR/.cache
+mkdir -p $OUT_DIR
+
+PREFIX=0155_00225_055_den
+
+# Pre-generate all the scene IDs
+NUMBERS=($(seq -w 200 2 1398))
+runjob() {
+    local id=$1
+
+    local scene=${PREFIX}_0${NUMBERS[id]}
+    echo "=== Running scene: $scene ==="
+
+    python train.py --config-name apps/scivis_3dgut_gs.yaml \
+        path=data/scivisgs/v0.1/nyx/$scene out_dir=$OUT_DIR > $OUT_DIR/nyx_${PREFIX}_$scene.log
+}
+
+# Command to submit: sbatch --array=0-1 benchmark/slurm_scivis.sh
+# 600 in total
+
+# for i in {3..200}; do
+#     runjob $i
+# done
+
+for i in {201..600}; do
+    runjob $i
+done
